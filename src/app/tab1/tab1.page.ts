f644b80c1ec1cb4f98fe7e0f9307993086771ae0
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2023-02-21 10:17:51 -0500

    fix(bug): use correct ionic version to reproduce bug

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,5 @@
+import { BehaviorSubject, delayWhen, from, interval, tap } from 'rxjs';
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,11 +9,22 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  public segments = ['general', 'detail', 'versions', 'rulings', 'other'];
-  public segment = 'detail';
+  public ui$ = from([{ '1': null }, { '1': undefined }, { '1': 2 }, { '1': 3 }])
+    .pipe(delayWhen(ui => ui[1] ? interval(Math.random() * 1000) : interval(10)))
+    .pipe(tap(c => console.warn('ui', c)));
 
-  updateSegment(value: string | undefined){
-    this.segment = value as string;
+  public segments$ = from([['general', 'detail', 'versions'], ['general', 'versions', 'rulings', 'other']])
+    .pipe(delayWhen(() => interval(Math.random() * 50)))
+    .pipe(tap(c => console.warn('segments', c)));
+
+  public _segment$ = new BehaviorSubject<string>('general');
+
+  public segment$ = this._segment$
+    .pipe(delayWhen(() => interval(Math.random() * 50)))
+    .pipe(tap(c => console.warn('segment', c)));
+
+  updateSegment2(value: string | null | undefined) {
+    this._segment$.next(value as string);
   }
 
 }
