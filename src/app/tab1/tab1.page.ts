1e456e4d444da21771f10fa8c558b6e37b1be545
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2023-03-22 17:51:38 -0400

    chore(ionic): demonstrate issue with ion-segment not firing value changes when appropriate

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject, delayWhen, from, interval, tap } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 import { Component } from '@angular/core';
 
@@ -9,22 +9,13 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  public ui$ = from([{ '1': null }, { '1': undefined }, { '1': 2 }, { '1': 3 }])
-    .pipe(delayWhen(ui => ui[1] ? interval(Math.random() * 1000) : interval(10)))
-    .pipe(tap(c => console.warn('ui', c)));
+  public segments = ['general', 'detail', 'versions'];
+  public segment$ = new BehaviorSubject<string>('general');
 
-  public segments$ = from([['general', 'detail', 'versions'], ['general', 'versions', 'rulings', 'other']])
-    .pipe(delayWhen(() => interval(Math.random() * 50)))
-    .pipe(tap(c => console.warn('segments', c)));
 
-  public _segment$ = new BehaviorSubject<string>('general');
-
-  public segment$ = this._segment$
-    .pipe(delayWhen(() => interval(Math.random() * 50)))
-    .pipe(tap(c => console.warn('segment', c)));
-
-  updateSegment2(value: string | null | undefined) {
-    this._segment$.next(value as string);
+  updateSegment(value: string | null | undefined) {
+    console.error('updating segment value', value)
+    this.segment$.next(value as string);
   }
 
 }
