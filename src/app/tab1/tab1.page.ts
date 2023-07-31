44e24ccbb8d8107a79323b42a137eabf4caf9da5
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2023-07-31 13:24:18 -0400

    feat(item-sliding): repro issue #23155

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 
 
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -9,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
   backHref = "/tabs/tab2?param=anything" 
+  optionsEnabled = true;
+
+  items: number[] | null = null;
+  constructor(private _cdr: ChangeDetectorRef) {
+    this._cdr.detach();
+  }
+
+  ngAfterViewInit() {
+    this.reload(true);
+  }
+
+  toggle() {
+    this.optionsEnabled = !this.optionsEnabled;
+    this._cdr.detectChanges();
+  }
+
+  reload(detect?: boolean) {
+    this.items = null;
+    if (detect) {
+      this._cdr.detectChanges();
+    }
+
+    window.setTimeout(() => {
+      this.items = new Array(1000);
+      for (var i = 0; i < this.items.length; i++) {
+        this.items[i] = i;
+      }
+      if (detect) {
+        this._cdr.detectChanges();
+      }
+    });
+  }
 }
